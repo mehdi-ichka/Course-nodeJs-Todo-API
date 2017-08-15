69ec2462f65a1cbf9ac1bb963de16a16cd3bcc7a
mehdi <mehdi.ichka@gmail.com>
2017-08-15 23:23:36 +0200

    Add delete script

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,12 +7,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('connect to mongodb server');
 
-    db.collection('Users').find({name: 'mehdi'}).toArray().then((docs) => {
-        console.log('Users');
-        console.log(JSON.stringify(docs, undefined, 2));
-    }, (err) => {
-        console.log('Unable to fetch data from Users', err);
+    // db.collection('Users').find({name: 'mehdi'}).toArray().then((docs) => {
+    //     console.log('Users');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch data from Users', err);
+    // })
+
+    db.collection('Users').findOneAndDelete({name: 'jen'}).then((result) => {
+        console.log(result);
     })
+
     // db.collection('Todos').count()
     // .then((count) => {
     //     console.log('Todos count: ', count);
